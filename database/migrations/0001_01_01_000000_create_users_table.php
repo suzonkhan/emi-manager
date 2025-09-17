ed3f318f2018d893ad638ac264ca84b3dbb4cbbd
touhidun <touhi13@gmail.com>
2025-09-17 23:19:31 +0600

    fix: Correct primary key definition for password_reset_tokens table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -44,9 +44,11 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email', 191)->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+            
+            $table->primary(['email']);
         });
 
         Schema::create('sessions', function (Blueprint $table) {
